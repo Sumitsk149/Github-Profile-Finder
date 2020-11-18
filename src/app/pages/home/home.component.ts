4b5027f215a64380142df90c8f16c62d77f2b626
Sumit <ssk149@gmail.com>
2020-11-18 19:03:21 +0530

    Home Page and Error Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+
+import { GithubService } from 'src/app/services/github.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  user = null;
+  userName: string;
+  error = null;
+
+  constructor(
+    private ref: ChangeDetectorRef,
+    private githubService: GithubService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  handleFind() {
+    this.githubService.getUserDetails(this.userName).subscribe(
+      (user) => {
+        this.user = user;
+        this.error = null;
+        this.ref.detectChanges();
+      },
+      (err) => {
+        this.user = null;
+        this.error = "User not found";
+        this.ref.detectChanges();
+      }
+    )
+  }
 }
